171729e52ccfd1455adfe57dd8121bc4ae425735
Logan Hasson <logan.hasson@gmail.com>
2015-05-26 13:05:34 -0400

    More requires

--- lib/learn_web.rb
+++ lib/learn_web.rb
@@ -1,8 +1,9 @@
 require 'learn_web/version'
+require 'learn_web/attribute_populatable'
 require 'learn_web/client'
 require 'learn_web/client/me'
 require 'learn_web/client/pull_request_response'
-require 'learn_web/attribute_populatable'
+require 'learn_web/client/current_lesson'
 
 module LearnWeb
 end
