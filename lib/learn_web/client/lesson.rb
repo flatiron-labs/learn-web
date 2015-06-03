52c07bdbe278afaa8ce48827b193efc890931fc3
Logan Hasson <logan.hasson@gmail.com>
2015-06-03 15:36:33 -0400

    Get next lesson

--- lib/learn_web/client/lesson.rb
+++ lib/learn_web/client/lesson.rb
@@ -1,10 +1,15 @@
 require 'learn_web/client/lesson/current_lesson'
+require 'learn_web/client/lesson/next_lesson'
 
 module LearnWeb
   class Client
     module Lesson
       def current_lesson_endpoint
         "#{API_ROOT}/users/current_lesson"
+      end
+
+      def next_lesson_endpoint
+        "#{API_ROOT}/users/next_lesson"
       end
 
       def current_lesson
@@ -14,6 +19,15 @@ module LearnWeb
         end
 
         LearnWeb::Client::Lesson::CurrentLesson.new(response)
+      end
+
+      def next_lesson
+        response = @conn.get do |req|
+          req.url next_lesson_endpoint
+          req.headers['Authorization'] = "Bearer #{token}"
+        end
+
+        LearnWeb::Client::Lesson::NextLesson.new(response)
       end
     end
   end
