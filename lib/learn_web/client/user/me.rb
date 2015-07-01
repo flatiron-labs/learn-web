d0ad83967a024eb655b048d2a5cd48db1d54469a
Logan Hasson <logan.hasson@gmail.com>
2015-07-01 11:57:02 -0400

    Add exit code so we can break out of the execution flow more easily

--- lib/learn_web/client/user/me.rb
+++ lib/learn_web/client/user/me.rb
@@ -24,13 +24,13 @@ module LearnWeb
             case response.status
             when 401
               puts "It seems your OAuth token is incorrect. Please re-run config with: learn reset"
-              exit
+              exit 1
             when 500
               puts "Something went wrong. Please try again."
-              exit
+              exit 1
             else
               puts "Something went wrong. Please try again."
-              exit
+              exit 1
             end
           end
 
