ec880ee30215f80dfcd2771cc54a7f80f0b79ddf
Logan Hasson <logan.hasson@gmail.com>
2015-09-09 12:12:31 -0400

    Add Verification interface

--- lib/learn_web/client/environment.rb
+++ lib/learn_web/client/environment.rb
@@ -1,4 +1,5 @@
 require 'learn_web/client/environment/setup_list'
+require 'learn_web/client/environment/verification'
 
 module LearnWeb
   class Client
@@ -7,10 +8,23 @@ module LearnWeb
         "#{API_ROOT}/environmentalizer/steps"
       end
 
+      def verification_endpoint
+        "#{API_ROOT}/gem_verifications"
+      end
+
       def environment_setup_list
         response = get(environment_setup_list_endpoint)
 
         LearnWeb::Client::Environment::SetupList.new(response)
+      end
+
+      def verify_environment
+        response = post(
+          verification_endpoint,
+          headers: { 'Authorization' => "Bearer #{token}" }
+        )
+
+        LearnWeb::Client::Environment::Verification.new(reponse)
       end
     end
   end
--- lib/learn_web/version.rb
+++ lib/learn_web/version.rb
@@ -1,3 +1,3 @@
 module LearnWeb
-  VERSION = '1.3.0'
+  VERSION = '1.4.0'
 end
