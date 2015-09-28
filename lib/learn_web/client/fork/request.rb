8060650dde44bfed7e230a287908c22ba4a91251
Logan Hasson <logan.hasson@gmail.com>
2015-09-28 17:18:32 -0400

    Error handle unverified emails

--- lib/learn_web/client/fork/request.rb
+++ lib/learn_web/client/fork/request.rb
@@ -19,6 +19,10 @@ module LearnWeb
           begin
             body = Oj.load(response.body, symbol_keys: true)
             self.message = body[:message]
+
+            if self.message && self.message.match(/verified your email/)
+              abort self.message
+            end
           rescue
             self.message = 'Sorry, something went wrong. Please try again.'
           end
